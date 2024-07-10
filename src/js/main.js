de4e60258954d2941a1785efaac6a0074f0b66f9
@cegn <cegn@senado.leg.br>
2024-07-10 20:33:58 -0300

    navbar full end

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.addEventListener('scroll', function() {
+  let header = document.querySelector('header')
+  header.classList.toggle('scrolling-on', window.scrollY > 0)
+})
