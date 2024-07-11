51cef1f26dacd2f8daf14f2920dcda300441e4f1
@cegn <cegn@senado.leg.br>
2024-07-11 14:10:21 -0300

    frontend em 95%

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,23 @@ window.addEventListener('scroll', function() {
   let header = document.querySelector('header')
   header.classList.toggle('scrolling-on', window.scrollY > 0)
 })
+
+// Observa o contador, quando é visto inicia a contagem
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const square = entry.target.querySelector(".status-fire-js");
+
+    if (entry.isIntersecting) {
+      square.classList.add("animation");
+      return; // if we added the class, exit the function
+    }
+
+    // We're not intersecting, so remove the class!
+    square.classList.remove("animation");
+  });
+});
+
+let elements = document.querySelectorAll(".count-wrapper");
+for (let element of elements) {
+  observer.observe(element);
+}
