3c03e2c4f539b1d051c4c58a8bc432dc216c1c90
@cegn <cegn@senado.leg.br>
2024-07-23 18:19:40 -0300

    frontend finalizado

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+import '/src/js/lightbox.js'
 
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.addEventListener('scroll', function() {
@@ -10,19 +11,19 @@ window.addEventListener('scroll', function() {
 // Observa o contador, quando é visto inicia a contagem
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    const square = entry.target.querySelector(".status-fire-js");
+    const square = entry.target.querySelector(".status-fire-js")
 
     if (entry.isIntersecting) {
-      square.classList.add("animation");
-      return; // if we added the class, exit the function
+      square.classList.add("animation")
+      return // if we added the class, exit the function
     }
 
     // We're not intersecting, so remove the class!
-    square.classList.remove("animation");
-  });
-});
+    square.classList.remove("animation")
+  })
+})
 
-let elements = document.querySelectorAll(".count-wrapper");
+let elements = document.querySelectorAll(".count-wrapper")
 for (let element of elements) {
-  observer.observe(element);
+  observer.observe(element)
 }
